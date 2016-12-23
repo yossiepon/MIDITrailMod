6e6f46e7948344e12e9fdfea94eb3ad0f8110ebc
yoshy <yoshy@users.sourceforge.jp>
2016-12-24 04:22:35 +0900

    [FIX] Aboutダイアログのバージョン情報の表示を修正

--- MIDITrail/MIDITrailVersion.h
+++ MIDITrail/MIDITrailVersion.h
@@ -16,8 +16,8 @@
 //******************************************************************************
 
 //バージョン文字列
-#define MIDITRAIL_VERSION_STRING_X86  _T("Version 1.2.2 (32bit) mod. yossiepon_20161223")
-#define MIDITRAIL_VERSION_STRING_X64  _T("Version 1.2.2 (64bit) mod. yossiepon_20161223")
+#define MIDITRAIL_VERSION_STRING_X86  _T("1.2.2 (x86), mod. yossiepon_20161223")
+#define MIDITRAIL_VERSION_STRING_X64  _T("1.2.2 (x64), mod. yossiepon_20161223")
 
 //コピーライト
 #define MIDITRAIL_COPYRIGHT          _T("Copyright (C) 2010-2016 WADA Masashi");
